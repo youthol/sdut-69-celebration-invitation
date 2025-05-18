df3368cdb338be0584df4d68544aa3d579c38707
Bulbul <1079729701@qq.com>
2025-05-18 18:30:59 +0800

    更新后端接口

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,8 @@
 // http.js
 import axios from 'axios'
 
-const baseURL = 'http://127.0.0.1:8000/api/'
+// const baseURL = 'http://127.0.0.1:8000/api/celebration/'
+const baseURL = 'https://youtholapi.bulbul559.cn/api/celebration/'
 
 export const http = axios.create({
   baseURL: baseURL,
